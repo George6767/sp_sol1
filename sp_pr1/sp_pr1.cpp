ab4fef4a5091e9029c6f9694d65a6b346d1ffef2
George6767 <129250696+George6767@users.noreply.github.com>
2023-07-04 16:12:56 +0300

    3.6 worked

--- sp_pr1/sp_pr1.cpp
+++ sp_pr1/sp_pr1.cpp
@@ -1,8 +1,7 @@
 #include <Windows.h>
 #include "sp_pr1.h"
-
-
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpszCmdLine, int nCmdShow)
 {
 	LPCTSTR lpszHelloText = MESSAGE_TEXT;
 	LPCTSTR lpszHelloWndTitle = MESSAGE_TITLE;
